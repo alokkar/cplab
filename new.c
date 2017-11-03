e4418793cd91a468a0f32ba45204ff6937e71f15
alokkar <32328732+alokkar@users.noreply.github.com>
2017-11-03 11:34:32 +0000

    Add files via upload

--- new.c
+++ new.c
@@ -18,31 +18,11 @@ void refill(int row, int col,int n)
 				mat[i][j]+=1;
 	return;
 }
-int solve(int n,int i)
-{
-	int t,j;
-	if(i==n)
-		return 1;
-	else
-	{
-		for(j=0;j<n;j++)
-			if(mat[i][j]==0)
-			{
-				attack(i,j,n);
-				t=solve(n,i+1);
-				if(t==1)
-					return 1;
-				else
-					refill(i,j,n);
-			}
-	}
-	return -1;
-}
-int main()
-{
-	int n,i,j;
-	scanf("%d",&n);
-	int p=solve(n,0);
+
+
+void print_final(int n){
+
+	int i,j;
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
@@ -52,5 +32,34 @@ int main()
 				printf("0 ");
 		printf("\n");
 	}
+	printf("\n");
+
+}
+void solve(int n,int i)
+{
+	int t,j,p,q;
+	if(i==n){
+		print_final(n);
+		return;
+	}
+	else
+	{
+		for(j=0;j<n;j++){
+			if(mat[i][j]==0)
+			{
+				attack(i,j,n);
+				solve(n,i+1);
+				refill(i,j,n);
+			}
+		}
+
+		return;
+	}
+}
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	solve(n,0);
 	return 0;
 }
